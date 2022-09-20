becc91872c79b3ed287d59b9e5664d48d419f805
PhilippMaschi <phil.masch@hotmail.com>
2022-09-20 10:07:55 +0200

    cluster work ongoing

--- cluster_load_profiles.py
+++ cluster_load_profiles.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 import os
-
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 class LoadProfileLoader:
@@ -29,36 +29,54 @@ class LoadProfileLoader:
         pass
 
     def read_load_profiles(self, csv_names: List[str]) -> pd.DataFrame:
-        big_table = pd.DataFrame(columns=["date", "hour"])
+        big_table = pd.DataFrame(columns=["date"])
         for i, name in enumerate(csv_names):
             file = Path(self.path_2_files) / Path(name)
             load = pd.read_csv(file, sep=";", decimal=",").loc[:, ["FECHA(YYYY-MM-DD o DD/MM/YYY)", "HORA(h)", "A+(Wh)"]]
             load.columns = ["date", "hour", "load"]  # rename the columns
             # normalize the load
             big_table.loc[:, f"load_{name}"] = self.normalize_load(load.load)
+            time_series = self.create_timestamp(load.loc[:, "date"], load.loc[:, "hour"])
             if i == 0:
-                big_table.loc[]
-            # check if the timestamps are the same
-            # add all the tables to one big dataframe
-            big_table = pd.concat([big_table, load])
+                big_table.loc[:, "date"] = time_series
+            else:  # check if the data has the same dates
+                assert big_table.loc[:, "date"].all() == time_series.all(), "time data does not match"
 
         return big_table
 
-    def create_timestamp(self, df: pd.DataFrame) -> pd.DataFrame:
+    def create_timestamp(self, date: pd.Series, hour: pd.Series) -> pd.DataFrame:
         """creates a timestamp in the 'date' column so we can cluster the data more easily"""
-        df.loc[:, "date"] = pd.to_datetime(df.date) + pd.to_timedelta(df.hour, unit='h')
-        return df
+        return pd.to_datetime(date) + pd.to_timedelta(hour, unit='h')
 
 
+    def main(self) -> pd.DataFrame:
+        all_profiles = self.read_load_profiles(self.get_csv_names())
+        return all_profiles
 
 
+class Visualization:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def create_daily_loads(self):
+        """plits the dataframe data into daly load pieces"""
+
+        pass
+
+    def lineplot_all_loads(self):
+        melted_df = self.df.melt(id_vars="date")
+        sns.lineplot(data=melted_df, x="date", y="value", hue="variable")
+        plt.show()
+        pass
 
 
     def main(self):
-        all_profiles = self.read_load_profiles(self.get_csv_names())
-        df = self.create_timestamp(all_profiles)
+        self.lineplot_all_loads()
+
+
 
 
 
 if __name__ == "__main__":
-    LoadProfileLoader().main()
+    profiles = LoadProfileLoader().main()
+    Visualization(profiles).main()
