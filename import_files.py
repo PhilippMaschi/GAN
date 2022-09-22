3608c33aceb15b09a42f0162fc3cb550d55f52c0
LaFra <francescaconselvan@gmail.com>
2022-09-22 11:33:02 +0200

    Re-arrangeds the module cluster_load_profiles
    I have rearranged the module, so we can use it also to import other load profiles. I  have added a from categorical_to_numerical function, but I am not sure that we need it.
    I have greyed the normalize_load function. I would added in another module, like Data Preparation that comes after the Exploratory Data Analysis.
    Comments in the code

--- import_files.py
+++ import_files.py
@@ -1,38 +1,41 @@
 import pandas as pd
+from pandas import DataFrame
 import os
 import glob
 
-from pandas import DataFrame
-from pandas_profiling import ProfileReport
-import sweetviz as sv
-from autoviz.AutoViz_Class import AutoViz_Class
 
-
-class LoadFiles:
+class FileLoader:
     def __init__(self, path):
         self.path = path
+        self.data = None
 
     def read_csv(self) -> pd.DataFrame:
+        self.load_data()
+        self.categorical()
+        return self.data
+
+    def load_data(self, file_name, column_list=[]) -> pd.DataFrame:
         csv_files = glob.glob(os.path.join(self.path, '*.csv'))
-        data = pd.concat([pd.read_csv(f, delimiter=';', index_col=None, ) for f in csv_files])
-        data.columns = ['date', 'hour', 'consumed energy', 'exported energy',
-                        'reactive energy Q1', 'reactive energy Q2', 'reactive energy Q3',
-                        'reactive energy Q4', 'contacted power P1', 'contacted power P2',
-                        'contacted power P3', 'contacted power P4', 'contacted power P5',
-                        'contacted power P6', 'no name']
-        data.to_csv('concatenated_csv.csv', index=False)
+        self.data = pd.concat([pd.read_csv(f, delimiter=';', index_col=None, ) for f in csv_files])
+        self.data.columns = column_list
+        self.data.to_csv(file_name, index=False)
 
-        cat_columns = data.select_dtypes(['object']).columns  # 'date', 'consumed energy'
-        data[cat_columns] = data[cat_columns].apply(lambda x: pd.factorize(x)[0])
-        return data
+    def categorical(self):
+        cat_columns = self.data.select_dtypes(['object']).columns  # 'date', 'consumed energy'
+        self.data[cat_columns] = self.data[cat_columns].apply(lambda x: pd.factorize(x)[0])
 
 
-path = r'C:\Users\FrancescaConselvan\Desktop\MODERATE\datasets\Enercoop'
+if __name__ == "__main__":
+    path = r'C:\Users\FrancescaConselvan\Desktop\MODERATE\datasets\Enercoop'
+    df = FileLoader(path)
+    data: DataFrame = df.refine_data(column_list=df)
+    print(df.data)
 
-df = LoadFiles(path)
-data: DataFrame = df.read_csv()
+csv_file = FileLoader(path)
 
-
-
-
-
+path_FC = r'C:\Users\FrancescaConselvan\Desktop\MODERATE\datasets\Enercoop'
+columns_name = ['date', 'hour', 'consumed energy', 'exported energy',
+                'reactive energy Q1', 'reactive energy Q2', 'reactive energy Q3',
+                'reactive energy Q4', 'contacted power P1', 'contacted power P2',
+                'contacted power P3', 'contacted power P4', 'contacted power P5',
+                'contacted power P6', 'no name']
