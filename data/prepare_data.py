4640b6f05d102dea4b7d9965ff48730d497f11d6
PhilippMaschi <phil.masch@hotmail.com>
2022-11-11 18:35:55 +0100

    prepare_data.py: adopting to new data

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def create_timestamp(df, date_column) -> pd.DataFrame:
     """creates a timestamp in the 'date' column so we can cluster the data more easily"""
-    df.loc[:, date_column] = pd.to_datetime(df.date) + pd.to_timedelta(df.hour, unit='h')
+    df.loc[:, date_column] = pd.to_datetime(df.Date) + pd.to_timedelta(df.Hour, unit='h')
     return df
 
 
@@ -34,11 +34,11 @@ def normalize_all_loads(df: pd.DataFrame) -> pd.DataFrame:
     @param df: dataframe with load profiles
 
     """
-    columns2drop = [name for name in df.columns if not "Wh" in name]  # define columns that are not normalized
-    date = df.loc[:, "date"]  # save date column
+    columns2drop = ["Date"]  # define columns that are not normalized
+    date = df.loc[:, "Date"]  # save date column
     normalized = MinMaxScaler().fit_transform(df.drop(columns=columns2drop))  # normalize data
     normalized_df = pd.DataFrame(normalized)  # to pd dataframe
-    normalized_df.insert(loc=0, column="date", value=date)  # insert the date column at first position
+    normalized_df.insert(loc=0, column="Date", value=date)  # insert the date column at first position
     normalized_df.columns = df.columns  # get the column names back
     return normalized_df
 
@@ -62,30 +62,19 @@ def extract_month_name_from_datetime(df: pd.DataFrame) -> list:
         11: "November",
         12: "December"
     }
-    names = [month_dict[i] for i in df.loc[:, "date"].dt.month]
+    names = [month_dict[i] for i in df.loc[:, "Date"].dt.month]
     return names
-
-
-def differentiate_positive_negative_loads(df: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
-    """
-    splits the dataframe into two frames, one having only the positive loads and one the negative
-    @rtype: return 2 dataframes. First dataframe is the consumption, Second is the prosuming data
-    """
-    columns_positive = [name for name in df.columns if "+" in name]
-    columns_negative = [name for name in df.columns if "-" in name]
-
-    return df.drop(columns=columns_negative), df.drop(columns=columns_positive)
 
 
 def add_hour_of_the_day_to_df(df: pd.DataFrame) -> pd.DataFrame:
     """ this function adds the hour of the day based on the timestamp column "date" """
-    df.loc[:, "hour"] = df.loc[:, "date"].dt.hour
+    df.loc[:, "Hour"] = df.loc[:, "Date"].dt.hour
     return df
 
 
 def add_day_of_the_month_to_df(df: pd.DataFrame) -> pd.DataFrame:
     """ this function adds the hour of the day based on the timestamp column "date" """
-    df.loc[:, "day"] = df.loc[:, "date"].dt.day
+    df.loc[:, "Day"] = df.loc[:, "Date"].dt.day
     return df
 
 
@@ -107,6 +96,7 @@ def sort_columns_months(df: pd.DataFrame) -> pd.DataFrame:
     ]
     df = df[column_names]
     return df
+
 
 def define_float_type(df: pd.DataFrame) -> pd.DataFrame:
     """ the data is converted to flaot32 in order to save memory and increase computational speed"""
