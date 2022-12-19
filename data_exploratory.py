ffb222715b8cb947ae64adb33d6e13820c215a32
LaFra <francescaconselvan@gmail.com>
2022-12-19 12:55:42 +0100

    data_exploratory

--- data_exploratory.py
+++ data_exploratory.py
@@ -1,56 +1,44 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+
 from pandas_profiling import ProfileReport
 import sweetviz as sv
 
-
-########## PROFILES ##########
-####################################
-def pandas_profiling(data, report_title, path, report_name):
-    profile = ProfileReport(data, title=report_title)
-    profile.to_file(output_file=path + "/" + report_name + ".html")
+import matplotlib.pyplot as plt
 
 
-def sweet_viz(data, path, report_title):
-    report = sv.analyze(data)
-    report.show_html(path + "/" + report_title + ".html")
+class Profiles:
 
+    def __init__(self, filename, load_profile_carrier):
+        self.df = pd.read_parquet(filename)
+        self.report_path = r"C:/Users/FrancescaConselvan/Documents/MODERATE/results/EDA reports"
+        self.figure_path = r"C:/Users/FrancescaConselvan/Documents/MODERATE/results/figures"
+        self.title = load_profile_carrier
+        print(self.df.describe())
 
-########## MISSING VALUES ##########
-####################################
-def missing_values(df):
-    tot_missing = df.isnull().sum().sort_values(ascending=False)
-    percent_missing = (df.isnull().sum() / df.isnull().count()).sort_values(ascending=False)
-    missing_data = pd.concat([tot_missing, percent_missing], axis=1, keys=['Total', 'Percent'])
-    return missing_data
+    def pandas_profiling(self):
+        pandas_profile = ProfileReport(self.df, title=self.title)
+        pandas_profile.to_file(output_file=self.report_path + "/" + self.title + ".html")
+        return pandas_profile
 
+    def sweet_viz_report(self):
+        sweetviz_report = sv.analyze(self.df)
+        sweetviz_report.show_html(self.report_path + "/" + self.title + ".html")
+        return sweetviz_report
 
-def drop_missing_values(col_with_missing_val):
-    data_no_missing_val = data.drop(columns=col_with_missing_val, axis=1)
-    data_no_missing_val.to_csv('data_clean.csv', index=False)
-    return data_no_missing_val
+    def outliers_boxplot(self):
+        fig = plt.figure()
+        box_plot = self.df.plot(kind="box", subplots=True, layout=(5, 3), figsize=(10, 10))
+        plt.savefig(self.figure_path + "/" + self.title + ".jpg")
+        return plt.show()
 
-
-def null_values(df):
-    for column_name in df.columns:
-        column = df[column_name]
-        count = (column == 0).sum()
-        print('Count of zer0s', column_name, ':', count)
-
-
-########## OUTLIERS ##########
-####################################
-def outliers_boxplot(data, path, fig_name):
-    fig = plt.figure()
-    bp = data.plot(kind='box', subplots=True, layout=(5, 3), figsize=(10, 10))
-    plt.savefig(path + "/" + fig_name + ".jpg")
-    return plt.show()
+    def missing_values(self):
+        tot_missing = self.df.isnull().sum().sort_values(ascending=False)
+        percent_missing = (self.df.isnull().sum() / self.df.isnull().count()).sort_values(ascending=False)
+        missing_data = pd.concat([tot_missing, percent_missing], axis=1, keys=['Total', 'Percent'])
+        missing_data.to_excel(self.report_path + "/" + self.title +".xlsx" )
+        return missing_data
 
 
 if __name__ == "__main__":
-  pandas_profiling()
-  sweet_viz()
-  missing_values()
-  null_values()
-  outliers_boxplot()
+    Profiles()
+
--- prepare_data.py
+++ prepare_data.py
@@ -9,3 +9,8 @@ def from_cat_to_num(df) -> pd.DataFrame:
     cat_columns = df.select_dtypes(['object']).columns
     df[cat_columns] = df[cat_columns].apply(lambda x: pd.factorize(x)[0])
     return df
+
+def drop_missing_values(col_with_missing_val):
+    data_no_missing_val = data.drop(columns=col_with_missing_val, axis=1)
+    data_no_missing_val.to_csv('data_clean.csv', index=False)
+    return data_no_missing_val
