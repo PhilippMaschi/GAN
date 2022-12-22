2b129f14f34bee5e14599ab06c95970f55b402a8
LaFra <francescaconselvan@gmail.com>
2022-12-22 17:04:34 +0100

    synthetic data

--- import_data.py
+++ import_data.py
@@ -2,8 +2,12 @@ import pandas as pd
 import pyarrow
 import pathlib
 
-path_file = r"C:/Users/FrancescaConselvan/Dropbox/MODERATE/Enercoop"
-data = pd.read_parquet(path_file+"/ENERCOOP_load_profiles.parquet.gzip")
+
+path_files = r"C:/Users/FrancescaConselvan/Dropbox/MODERATE/Enercoop"
+
+data = pd.read_parquet(path_files+"/ENERCOOP_load_profiles.parquet.gzip")
 
 df = pd.read_json(r"C:/Users/FrancescaConselvan/Dropbox/MODERATE/Enercoop/all_load_profiles.json")
 
+data_reduced = data.iloc[:100000]
+data_reduced.to_csv(path_files + "/data_reduced.csv")
--- prepare_data.py
+++ prepare_data.py
@@ -1,16 +1,48 @@
-def create_timestamp(df, date_column) -> pd.DataFrame:
-    """creates a timestamp in the 'date' column so we can cluster the data more easily"""
-    df.loc[:, date_column] = pd.to_datetime(df.date) + pd.to_timedelta(df.hour, unit='h')
-    return df
+import pandas as pd
+from pathlib import Path
+import glob
+import os
+
+class clean_dataframe:
+
+    def __init__(self, path_to_csv):
+        path = path_to_csv
+
+    def concatenate_csv(path):
+
+        files = Path(path).glob("*.csv")
+        dfs = [pd.read_csv(f, delimiter=";", low_memory=False) for f in files]
+        df = pd.concat(dfs, ignore_index=True)
+        df.to_csv(path+".csv")
+        return  df.info()
+
+    def columns_name ():
+
+    def create_timestamp(df, date_column) -> pd.DataFrame:
+        """creates a timestamp in the 'date' column so we can cluster the data more easily"""
+        df.loc[:, date_column] = pd.to_datetime(df.date) + pd.to_timedelta(df.hour, unit='h')
+        return df
+
+    def drop_missing_values(col_with_missing_val):
+        data_no_missing_val = data.drop(columns=col_with_missing_val, axis=1)
+        data_no_missing_val.to_csv('data_clean.csv', index=False)
+        return data_no_missing_val
+
+    def from_cat_to_num(df) -> pd.DataFrame:
+        cat_columns = df.select_dtypes(['object']).columns
+        df[cat_columns] = df[cat_columns].apply(lambda x: pd.factorize(x)[0])
+        return df
+
+
+if __name__ == "__main__":
+    path = path_file + "/csv"
+    df = import_csv(path_csv=path_to_csv)
+
+
+
+
+
 
 
 # function to convert categorical variables in numerical. Necessary ???
-def from_cat_to_num(df) -> pd.DataFrame:
-    cat_columns = df.select_dtypes(['object']).columns
-    df[cat_columns] = df[cat_columns].apply(lambda x: pd.factorize(x)[0])
-    return df
 
-def drop_missing_values(col_with_missing_val):
-    data_no_missing_val = data.drop(columns=col_with_missing_val, axis=1)
-    data_no_missing_val.to_csv('data_clean.csv', index=False)
-    return data_no_missing_val
