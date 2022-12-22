e5987d75d06f0d0e5710620a9fdbcb465567948b
LaFra <francescaconselvan@gmail.com>
2022-12-22 13:13:53 +0100

    bye bye

--- import_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pyarrow
-import pathlib
-
-path_file = r"C:/Users/FrancescaConselvan/Dropbox/MODERATE/Enercoop"
-data = pd.read_parquet(path_file+"/ENERCOOP_load_profiles.parquet.gzip", chunked=100000000)
-
-print("Done")
-
