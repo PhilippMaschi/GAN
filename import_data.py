5f83cdcbfa73b82736944c5732089eb33cec4fa8
LaFra <francescaconselvan@gmail.com>
2023-01-10 14:20:03 +0100

    synthetic data

--- import_data.py
+++ import_data.py
@@ -5,12 +5,6 @@ import pathlib
 
 path_files = r"C:/Users/FrancescaConselvan/Dropbox/MODERATE/Enercoop"
 
-data_all = pd.read_csv(path_files + "/data_reduced.csv")
-data = pd.read_parquet(path_files + "/all_data.gzip")
+data = pd.read_parquet(path_files+"/ENERCOOP_load_profiles.parquet.gzip")
 
-data_all = pd.read_parquet(path_files+"/ENERCOOP_load_profiles.parquet.gzip")
-data_json_all = pd.read_json(r"C:/Users/FrancescaConselvan/Dropbox/MODERATE/Enercoop/all_load_profiles.json")
-
-data_reduced = data_all.iloc[:100000]
-data_reduced.to_csv(path_files + "/data_reduced.csv")
 
--- prepare_data.py
+++ prepare_data.py
@@ -3,20 +3,8 @@ from pathlib import Path
 import glob
 import os
 
-class clean_dataframe:
+class CleanDataframe:
 
-    def __init__(self, path_to_csv):
-        path = path_to_csv
-
-    def concatenate_csv(path):
-
-        files = Path(path).glob("*.csv")
-        dfs = [pd.read_csv(f, delimiter=";", low_memory=False) for f in files]
-        df = pd.concat(dfs, ignore_index=True)
-        df.to_csv(path+".csv")
-        return  df.info()
-
-    def columns_name ():
 
     def create_timestamp(df, date_column) -> pd.DataFrame:
         """creates a timestamp in the 'date' column so we can cluster the data more easily"""
@@ -24,7 +12,7 @@ class clean_dataframe:
         return df
 
     def drop_missing_values(col_with_missing_val):
-        data_no_missing_val = data.drop(columns=col_with_missing_val, axis=1)
+        data_no_missing_val = df.drop(columns=col_with_missing_val, axis=1)
         data_no_missing_val.to_csv('data_clean.csv', index=False)
         return data_no_missing_val
 
@@ -32,6 +20,19 @@ class clean_dataframe:
         cat_columns = df.select_dtypes(['object']).columns
         df[cat_columns] = df[cat_columns].apply(lambda x: pd.factorize(x)[0])
         return df
+
+    def normalize_all_loads(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        @rtype: return the dataframe with all loads normalized between 0 and 1
+        @param df: dataframe with load profiles
+        """
+        columns2drop = ["Date"]  # define columns that are not normalized
+        date = df.loc[:, "Date"]  # save date column
+        normalized = MinMaxScaler().fit_transform(df.drop(columns=columns2drop))  # normalize data
+        normalized_df = pd.DataFrame(normalized)  # to pd dataframe
+        normalized_df.insert(loc=0, column="Date", value=date)  # insert the date column at first position
+        normalized_df.columns = df.columns  # get the column names back
+        return normalized_df
 
 if __name__ == "__main__":
     path = path_file + "/csv"
