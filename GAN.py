c17ef6295e0c218585b9ef05344bc59f151f21d1
LaFra <francescaconselvan@gmail.com>
2023-01-11 10:36:13 +0100

    DoppelGANger

--- GAN.py
+++ GAN.py
@@ -4,14 +4,33 @@ import matplotlib.pyplot as plt
 import torch as t
 from gretel_synthetics.timeseries_dgan.dgan import DGAN
 from gretel_synthetics.timeseries_dgan.config import DGANConfig, OutputType, Normalization
+#from import_data import data
+class Doppelgan:
 
-from import_data import df
+    def __init__(self, filename, outputdir, load_profile_carrier=None):
+        self.df = pd.read_parquet(filename)
+        self.df = self.df[["Date", "Consumed energy [Wh]"]]
 
-train_df = df[["Date", "Consumed energy [Wh]"]]
+        self.report_path = f"{outputdir}/results/EDA reports"
+        self.figure_path = f"{outputdir}/results/figures"
+        self.title = load_profile_carrier
+        print(self.df.describe())
 
-fig = plt.figure(figsize=(8,4))
-plt.plot(train_df["Date"], train_df["Consumed energy [Wh]"])
-plt.xticks(rotation=90)
-plt.ylabel("Consumed energy")
-plt.xlabel("Date")
-plt.show()
+    def plot_distribution (self):
+        fig = plt.figure(figsize=(8, 4))
+        plt.plot(self.df["Date"], self.df["Consumed energy [Wh]"])
+        plt.xticks(rotation=90)
+        plt.ylabel("Consumed energy")
+        plt.xlabel("Date")
+        plt.title(self.title)
+        plt.savefig(self.figure_path+"/data_GANs_distribution.jpg")
+        return plt.show()
+
+
+if __name__ == "__main__":
+
+    data = Doppelgan(filename = r"C:/Users/FrancescaConselvan/Dropbox/MODERATE/Enercoop//ENERCOOP_load_profiles.parquet.gzip",
+                     outputdir=r"C:/Users/FrancescaConselvan/Documents/MODERATE_GAN",
+                     load_profile_carrier="Enercoop")
+
+    data.plot_distribution()
