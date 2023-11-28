a01f6acf66c9aad31c02be45ba039b8a10af29c5
PhilippMaschi <phil.masch@hotmail.com>
2023-11-28 16:16:49 +0100

    visualization_script.py: minor

--- visualization_script.py
+++ visualization_script.py
@@ -6,7 +6,8 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import cryptpandas as crp
 from pathlib import Path
-
+from scipy.stats import ks_2samp
+from sdmetrics.timeseries import LSTMDetection
 
 def plot_pca_analysis(real_data, synthetic_data, output_path: Path):
     """
@@ -130,35 +131,35 @@ def plot_seasonal_daily_means(df_real: pd.DataFrame,
         season_real = season_groups_real.get_group(season)[["hour of the day"] + numeric_cols]
         season_synthetic = season_groups_synthetic.get_group(season)[["hour of the day"] + numeric_cols]
         # Filter the dataframe for the season and aggregate data by hour
-        seasonal_hourly_means_real = season_real.groupby("hour of the day").mean()
-        seasonal_hourly_std_real = season_real.groupby("hour of the day").std()
+        seasonal_hourly_means_real = season_real.groupby("hour of the day").mean().mean(axis=1)
+        seasonal_hourly_std_real = season_real.groupby("hour of the day").std().mean(axis=1)
 
-        seasonal_hourly_means_synthetic = season_synthetic.groupby("hour of the day").mean()
-        seasonal_hourly_std_synthetic = season_synthetic.groupby("hour of the day").std()
+        seasonal_hourly_means_synthetic = season_synthetic.groupby("hour of the day").mean().mean(axis=1)
+        seasonal_hourly_std_synthetic = season_synthetic.groupby("hour of the day").std().mean(axis=1)
 
         # Plot seasonal mean and standard deviation
         ax.plot(np.arange(24),
-                seasonal_hourly_means_real.mean(axis=1),
+                seasonal_hourly_means_real,
                 color="blue",
                 linewidth=2,
                 label=f'{season.capitalize()} Mean Real',
                 )
         ax.fill_between(np.arange(24),
-                        seasonal_hourly_means_real.mean(axis=1) - seasonal_hourly_std_real.mean(axis=1),
-                        seasonal_hourly_means_real.mean(axis=1) + seasonal_hourly_std_real.mean(axis=1),
+                        seasonal_hourly_means_real - seasonal_hourly_std_real,
+                        seasonal_hourly_means_real + seasonal_hourly_std_real,
                         alpha=0.3,
                         label=f'{season.capitalize()} Std Dev Real',
                         color="cyan")
 
         ax.plot(np.arange(24),
-                seasonal_hourly_means_synthetic.mean(axis=1),
+                seasonal_hourly_means_synthetic,
                 color="red",
                 linewidth=2,
                 label=f'{season.capitalize()} Mean Synthetic',
                 )
         ax.fill_between(np.arange(24),
-                        seasonal_hourly_means_synthetic.mean(axis=1) - seasonal_hourly_std_synthetic.mean(axis=1),
-                        seasonal_hourly_means_synthetic.mean(axis=1) + seasonal_hourly_std_synthetic.mean(axis=1),
+                        seasonal_hourly_means_synthetic - seasonal_hourly_std_synthetic,
+                        seasonal_hourly_means_synthetic + seasonal_hourly_std_synthetic,
                         alpha=0.3,
                         label=f'{season.capitalize()} Std Dev Synthetic',
                         color="lightcoral")
@@ -166,13 +167,59 @@ def plot_seasonal_daily_means(df_real: pd.DataFrame,
         # Formatting the seasonal plot
         ax.set_xlabel('Hour of Day')
         ax.set_ylabel('Mean Profile Value')
-        ax.set_title(f'Mean Hourly Profile for {season.capitalize()} and Custer')
+        ax.set_title(f'Mean Hourly Profile for {season.capitalize()}')
         ax.legend()
         # plt.xticks(range(0, 24))
         # ax.grid(True)
     plt.tight_layout()
     fig.savefig(output_path / f"Daily_Mean_Comparison.png")
     plt.close(fig)
+
+
+def compare_distributions(real_df,
+                          synthetic_df,
+                          output_path: Path,
+                          bins=50, ):
+    """
+    Compares the distributions of columns in two dataframes using histogram comparison
+    and the Kolmogorov-Smirnov test.
+
+    Parameters:
+    real_df (pd.DataFrame): Dataframe with real load profiles.
+    synthetic_df (pd.DataFrame): Dataframe with synthetic load profiles.
+    bins (int): Number of bins to use for histograms.
+
+    Returns:
+
+    """
+    if real_df.shape[1] != synthetic_df.shape[1]:
+        raise ValueError("Both dataframes must have the same number of columns.")
+
+    results = {}
+    numeric_cols = [col for col in df_real.columns if is_number(col)]
+    real = real_df[numeric_cols]
+    synthetic = synthetic_df[numeric_cols]
+    for col in real.columns:
+        if col not in synthetic:
+            raise ValueError(f"Column {col} not found in synthetic dataframe.")
+
+        real_data = real[col].dropna()
+        synthetic_data = synthetic[col].dropna()
+
+        # Kolmogorov-Smirnov test
+        ks_stat, ks_p_value = ks_2samp(real_data, synthetic_data)
+        results[col] = {'KS Statistic': ks_stat, 'KS p-value': ks_p_value}
+
+        # Histogram comparison
+        plt.figure(figsize=(10, 6))
+        plt.hist(real_data, bins=bins, alpha=0.5, label='Real')
+        plt.hist(synthetic_data, bins=bins, alpha=0.5, label='Synthetic')
+        plt.title(f'Histogram Comparison for {col}')
+        plt.xlabel('Value')
+        plt.ylabel('Frequency')
+        plt.legend()
+        plt.show()
+
 
 
 def load_all(clusterLabel: int):
@@ -191,9 +238,23 @@ def load_all(clusterLabel: int):
     return df_real, df_synthetic
 
 
+def run_sdm_lstm_detection_test(real_df, synthetic_df):
+    numeric = [col for col in df_real.columns if is_number(col)]
+
+    a = LSTMDetection.compute(real_data=df_real[numeric],
+                              synthetic_data=df_synthetic[numeric],
+                              metadata=df_real["timestamp"])
+
 if __name__ == "__main__":
     figures_output = Path(r"plots")
     df_real, df_synthetic = load_all(clusterLabel=1)
+    run_sdm_lstm_detection_test(real_df=df_real,
+                                synthetic_df=df_synthetic)
+    # compare_distributions(real_df=df_real,
+    #                       synthetic_df=df_synthetic,
+    #                       output_path=figures_output,
+    #                       bins=50)
+
     plot_seasonal_daily_means(df_real=df_real,
                               df_synthetic=df_synthetic,
                               output_path=figures_output)
