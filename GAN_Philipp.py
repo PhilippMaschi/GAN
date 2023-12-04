9e07ae9c3a43e016f03b70ceee02bd78aa557c51
PhilippMaschi <phil.masch@hotmail.com>
2023-12-04 14:29:25 +0100

    added GAN_Philipp.py and philipp_main.py

---
+++ GAN_Philipp.py
@@ -0,0 +1,243 @@
+import torch
+from torch import nn, cat, optim, full, randn, no_grad
+import pandas as pd
+from torch.profiler import profile, record_function, ProfilerActivity
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from time import perf_counter
+
+
+# import sys
+
+
+class Generator(nn.Module):
+    def __init__(self, dimLatent, featureCount, classCount, dimEmbedding):
+        super(Generator, self).__init__()
+        self.dimLatent = dimLatent  # Spalten von noise vector
+        self.featureCount = featureCount  # hours per day
+        self.classCount = classCount
+        self.dimEmbedding = dimEmbedding  # dimension of the embedding tensor
+        self.labelEmbedding = nn.Embedding(num_embeddings=self.classCount, embedding_dim=self.dimEmbedding)
+        self.model = nn.Sequential(
+            # 1st layer
+            nn.Linear(in_features=self.dimLatent + self.dimEmbedding, out_features=64),
+            nn.LeakyReLU(inplace=True),
+            # 2nd layer
+            nn.Dropout(0.2),
+            # 3rd layer
+            nn.Linear(in_features=64, out_features=128),
+            nn.LeakyReLU(inplace=True),
+            # 4th layer
+            nn.Linear(in_features=128, out_features=128),
+            nn.LeakyReLU(inplace=True),
+            # 5th layer
+            nn.Linear(in_features=128, out_features=128),
+            nn.LeakyReLU(inplace=True),
+            # 6th layer
+            nn.Dropout(0.1),
+            # 7th layer
+            nn.Linear(in_features=128, out_features=featureCount),
+            nn.Tanh()
+        )
+
+    def forward(self, noise, labels):
+        labels_ = self.labelEmbedding(labels)
+        # apply model to concatenated tensor (fixed label tensor + noise tensor) noise is added to columns: Rows stay the same
+        x = self.model(cat((labels_, noise), -1))
+        return x
+
+
+class Discriminator(nn.Module):
+    def __init__(self, featureCount, classCount, dimEmbedding):
+        super(Discriminator, self).__init__()
+        self.featureCount = featureCount
+        self.classCount = classCount
+        self.dimEmbedding = dimEmbedding
+        self.labelEmbedding = nn.Embedding(num_embeddings=self.classCount, embedding_dim=dimEmbedding)
+        self.model = nn.Sequential(
+            # 1st layer
+            nn.Linear(in_features=self.featureCount + self.dimEmbedding, out_features=128),
+            nn.LeakyReLU(inplace=True),
+            # 2nd layer
+            nn.Dropout(0.1),
+            # 3rd layer
+            nn.Linear(in_features=128, out_features=128),
+            nn.LeakyReLU(inplace=True),
+            # 4th layer
+            nn.Linear(in_features=128, out_features=128),
+            nn.LeakyReLU(inplace=True),
+            # 5th layer
+            nn.Linear(in_features=128, out_features=64),
+            nn.LeakyReLU(inplace=True),
+            # 6th layer
+            nn.Dropout(0.1),
+            # 7th layer
+            nn.Linear(in_features=64, out_features=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, data, labels):
+        labels_ = self.labelEmbedding(labels)
+        bool_ = self.model(cat((data, labels_), -1))
+        return bool_
+
+
+class GAN(object):
+    def __init__(self, name, device, batchSize, samples, labels, dimLatent, featureCount, classCount, dimEmbedding, lr,
+                 maxNorm, epochCount, testLabel=None, exampleCount=3):
+        self.name = name
+        self.device = device
+        self.batchSize = batchSize
+        self.samples = samples
+        self.labels = labels
+        self.dimLatent = dimLatent
+        self.featureCount = featureCount
+        self.classCount = classCount
+        self.dimEmbedding = dimEmbedding
+        self.lr = lr
+        self.maxNorm = maxNorm
+        self.epochCount = epochCount
+        self.testLabel = testLabel
+        self.exampleCount = exampleCount
+
+        # Scale data and create dataLoader
+        self.scaler = MinMaxScaler(feature_range=(-1, 1))
+        self.samplesScaled = self.scaler.fit_transform(samples.T).T
+        samples_ = torch.Tensor(self.samplesScaled)
+        labels_ = torch.Tensor(self.labels)
+        self.dataset = TensorDataset(samples_, labels_)
+        self.dataLoader = DataLoader(self.dataset, batch_size=self.batchSize, shuffle=False)  # True)
+
+        # Initialize generator
+        self.Gen = Generator(dimLatent, featureCount, classCount,
+                             dimEmbedding)  # classCount all profiles x days, not batched
+        self.Gen.to(self.device)
+
+        # Initialize discriminator
+        self.Dis = Discriminator(featureCount, classCount, dimEmbedding)
+        self.Dis.to(self.device)
+
+        # Initialize optimizers
+        self.optimGen = optim.Adam(params=self.Gen.parameters(), lr=self.lr)
+        self.optimDis = optim.Adam(params=self.Dis.parameters(), lr=self.lr)
+
+        # Initialize the loss function
+        self.criterion = nn.BCELoss()
+
+        self.df_loss = pd.DataFrame(
+            columns=[
+                'epoch',
+                'batch index',
+                'discriminator loss (real data)',
+                'discriminator loss (fake data)',
+                'discriminator loss',
+                'generator loss',
+                'discriminator gradient norm',
+                'generator gradient norm'
+            ])
+        self.iterCount = 0
+        if isinstance(self.testLabel, int):
+            self.noiseFixed = randn(self.exampleCount, dimLatent, device=device)
+            self.labelsFixed = full(size=(self.exampleCount,), fill_value=self.testLabel, device=self.device,
+                                    dtype=torch.int32)
+
+    def __labels__(self):
+        return self.labels
+
+    def train(self):
+        for epoch in tqdm(range(self.epochCount)):
+            for batchIdx, (data, target_) in enumerate(self.dataLoader):  # target = actual (real) label
+                data = data.to(device=self.device,
+                               dtype=torch.float32)  # rows: days x profiles (as provoded by dataLoader => length Batchsize)), columns hours per day
+                target = target_.to(device=self.device,
+                                    dtype=torch.int32)  # Index column vector: rows are days x profiles (as provoded by dataLoader => length Batchsize))
+
+                # Train discriminator with real data
+                tstamp_1 = perf_counter()
+                self.Dis.zero_grad()  # set the gradients to zero for every mini-batch
+                yReal = self.Dis(data, target)  # column vector: length as target train discriminator with real data, row vector: number of days x profiles
+                labelReal = full(size=(data.size(0), 1), fill_value=1, device=self.device,
+                                 dtype=torch.float32)  # Column vector a tensor containing only ones
+                lossDisReal = self.criterion(yReal, labelReal)  # calculate the loss : Single number?
+                lossDisReal.backward()  # calculate new gradients
+                # print(f'Train discriminator with real data: {perf_counter() - tstamp_1}')
+                # Train discriminator with fake data
+                tstamp_2 = perf_counter()
+                noise = randn(data.size(0), self.dimLatent,
+                              device=self.device)  # create a tensor filled with random numbers rows: Number of days, column dimLatent
+                randomLabelFake = target_.to(device=self.device,
+                                             dtype=torch.int32)  # torch.randint(low = 0, high = self.classCount, size = (data.size(0),), device = self.device)  #random labels needed in addition to the noise
+                labelFake = full(size=(data.size(0), 1), fill_value=0, device=self.device,
+                                 dtype=torch.float32)  # a tensor containing only zeros
+                xFake = self.Gen(noise, randomLabelFake)  # create fake data from noise + random labels with generator
+                yFake = self.Dis(xFake.detach(), randomLabelFake)  # let the discriminator label the fake data (`.detach()` creates a copy of the tensor)
+                lossDisFake = self.criterion(yFake, labelFake)
+                lossDisFake.backward()
+
+                lossDis = (lossDisReal + lossDisFake)  # compute the total discriminator loss
+                grad_norm_dis = torch.nn.utils.clip_grad_norm_(self.Dis.parameters(),
+                                                               max_norm=self.maxNorm)  # gradient clipping (large max_norm to avoid actual clipping)
+                self.optimDis.step()  # update the discriminator
+                # print(f'Train discriminator with fake data: {perf_counter() - tstamp_2}')
+
+                # Train generator (now that we fed the discriminator with fake data)
+                tstamp_3 = perf_counter()
+                self.Gen.zero_grad()
+                yFake_2 = self.Dis(xFake,
+                                   randomLabelFake)  # let the discriminator label the fake data (now that the discriminator is updated)
+                lossGen = self.criterion(yFake_2,
+                                         labelReal)  # calculate the generator loss (small if the discriminator thinks that `yFake_2 == labelReal`)
+                lossGen.backward()
+                grad_norm_gen = torch.nn.utils.clip_grad_norm_(self.Gen.parameters(), max_norm=self.maxNorm)
+                self.optimGen.step()
+                # print(f'Train generator (now that we fed the discriminator with fake data): {perf_counter() - tstamp_3}')
+
+                # Log the progress
+                tstamp_4 = perf_counter()
+                self.df_loss.loc[len(self.df_loss)] = [
+                    epoch,
+                    batchIdx,
+                    lossDisReal.detach().cpu().numpy(),
+                    lossDisFake.detach().cpu().numpy(),
+                    lossDis.detach().cpu().numpy(),
+                    lossGen.detach().cpu().numpy(),
+                    grad_norm_dis.detach().cpu().numpy(),
+                    grad_norm_gen.detach().cpu().numpy()
+                ]
+                if self.iterCount % max(1, int(self.epochCount * len(
+                        self.dataLoader) / 10)) == 0 or self.iterCount == self.epochCount * len(self.dataLoader) - 1:
+                    # print(f'training: {int(self.iterCount/(self.epochCount*len(self.dataLoader))*100)} %')
+                    if isinstance(self.testLabel, int):
+                        with no_grad():
+                            xFakeTest = self.Gen(self.noiseFixed, self.labelsFixed)
+                            yFakeTest = self.Dis(xFakeTest, self.labelsFixed)
+                            plt.figure(figsize=(4, 3), facecolor='w')
+                            plt.plot(xFakeTest.detach().cpu().numpy().T)
+                            plt.title(
+                                f'labels: {self.labelsFixed.cpu().numpy()}\ndiscriminator: {yFakeTest.detach().cpu().numpy().reshape(-1).round(4)}')
+                            plt.show();
+                self.iterCount += 1
+
+        del self.samples
+        del self.samplesScaled
+        del self.dataset
+        del self.dataLoader
+
+
+    def generate_sample(self):
+        synthSamples_list = []
+        for item in self.labels:
+            noise = randn(1, self.dimLatent, device=self.device)
+            label_ = full(size=(1,), fill_value=item, device=self.device, dtype=torch.int32)
+            sampleGen = self.Gen(noise, label_).detach().to_dense().cpu().numpy()
+            synthSamples_list.append(sampleGen)
+        synthSamples = np.vstack(synthSamples_list)
+        return synthSamples
+
+    def generate_scaled_sample(self):
+        synthSamples = self.generate_sample()
+        scaled_gen_sample = self.scaler.inverse_transform(synthSamples.T).T
+        return scaled_gen_sample
---
+++ philipp_main.py
@@ -0,0 +1,216 @@
+import cryptpandas as crp
+import os, sys, signal
+import getpass
+import pandas as pd
+import numpy as np
+
+import plotly.express as px
+import plotly.graph_objects as go
+import torch
+
+print(f'torch {torch.__version__}')
+from sklearn.preprocessing import minmax_scale, MinMaxScaler
+import json
+from pathlib import Path
+from torch.utils.data import TensorDataset, DataLoader
+import torch
+import matplotlib.pyplot as plt
+from datetime import date
+
+from data_manip import remove_incomplete_days
+from preproc import import_and_preprocess_data, create_and_add_datetime_features
+from GAN_Philipp import GAN
+from visualization_script import small_analysis
+from plot import plot_losses
+from plot import plot_synthetic_vs_real_samples
+import argparse
+
+import gc
+
+
+def conv_index_to_bins(index):
+    """Calculate bins to contain the index values.
+    The start and end bin boundaries are linearly extrapolated from
+    the two first and last values. The middle bin boundaries are
+    midpoints.
+
+    Example 1: [0, 1] -> [-0.5, 0.5, 1.5]
+    Example 2: [0, 1, 4] -> [-0.5, 0.5, 2.5, 5.5]
+    Example 3: [4, 1, 0] -> [5.5, 2.5, 0.5, -0.5]"""
+    assert index.is_monotonic_increasing or index.is_monotonic_decreasing
+
+    # the beginning and end values are guessed from first and last two
+    start = index[0] - (index[1] - index[0]) / 2
+    end = index[-1] + (index[-1] - index[-2]) / 2
+
+    # the middle values are the midpoints
+    middle = pd.DataFrame({'m1': index[:-1], 'p1': index[1:]})
+    middle = middle['m1'] + (middle['p1'] - middle['m1']) / 2
+
+    if isinstance(index, pd.DatetimeIndex):
+        idx = pd.DatetimeIndex(middle).union([start, end])
+    elif isinstance(index, (pd.Float64Index, pd.RangeIndex, pd.Int64Index)):
+        idx = pd.Float64Index(middle).union([start, end])
+    else:
+        print('Warning: guessing what to do with index type %s' %
+              type(index))
+        idx = pd.Float64Index(middle).union([start, end])
+
+    return idx.sort_values(ascending=index.is_monotonic_increasing)
+
+
+def calc_df_mesh(df):
+    """Calculate the two-dimensional bins to hold the index and
+    column values."""
+    return np.meshgrid(conv_index_to_bins(df.index),
+                       conv_index_to_bins(df.columns))
+
+
+def get_labels_for_cluster(cluster_number: int, filepath: Path) -> pd.DataFrame:
+    df_labels = pd.read_csv(os.path.join(filepath), sep=';')
+    df_labels['name'] = df_labels['name'].str.split('_', expand=True)[1]
+    return df_labels
+
+
+def load_labels_for_cluster(clusterLabel: int, filepath: Path, number_of_profiles: int):
+    df_labels = get_labels_for_cluster(cluster_number=clusterLabel, filepath=filepath)
+    labels = df_labels.loc[df_labels['labels'] == clusterLabel, 'name'].to_list()[:number_of_profiles]
+    return labels
+
+
+def is_number(s):
+    """
+    Check if the input string s is a number.
+
+    Parameters:
+    s (str): The string to check.
+
+    Returns:
+    bool: True if s is a number, False otherwise.
+    """
+    try:
+        float(s)  # for int, long and float
+    except ValueError:
+        return False
+    return True
+
+
+def create_training_data(all_profiles: pd.DataFrame, labels: list):
+    # filter the total amount of profiles:
+    meta_data_cols = [col for col in all_profiles.columns if not is_number(col)]
+    df_profiles = all_profiles[meta_data_cols + labels]
+
+    df_melted = df_profiles.melt(id_vars=df_profiles[meta_data_cols],
+                                 value_vars=df_profiles[labels],
+                                 var_name='profile')
+    # Rows: 1 entry for each day and profile, columns: hour of day
+    df_training = df_melted.pivot_table(values='value',
+                                        index=['profile', 'date'],
+                                        columns='hour of the day')
+    return df_training
+
+
+def main(password_, number_of_profiles, clusterLabel: int, label_csv_filename: str, dim_emmbedded=1000, ):
+    # Data import
+    GAN_data_path = Path().absolute().parent / 'GAN_data'
+    # all load profiles:
+    df_loadProfiles = crp.read_encrypted(path=os.path.join(GAN_data_path, 'all_profiles.crypt'), password=password_)
+
+    # filter the total amount of profiles:
+    labels = load_labels_for_cluster(clusterLabel=clusterLabel,
+                                     filepath=GAN_data_path / 'DBSCAN_15_clusters_labels.csv',
+                                     number_of_profiles=number_of_profiles)
+    print(f"number of profiles: {len(labels)}")
+
+    training_df = create_training_data(all_profiles=df_loadProfiles, labels=labels)
+    print(f"Shape training data: {training_df.shape} ")
+
+    # Create and scale samples and labels
+    training_samples = training_df.to_numpy().astype("f4")
+    # each day gets the same label without considering which profile
+    training_labels = np.tile(np.array(range(395)), number_of_profiles)
+
+    # Configure GAN
+    if 1 and torch.cuda.is_available():
+        device = torch.device('cuda:0')
+        print('GPU is used.')
+    else:
+        device = torch.device('cpu')
+        print('CPU is used.')
+
+    batchSize = int(training_samples.shape[0] / number_of_profiles)
+    dimLatent = 10
+    featureCount = training_samples.shape[1]  # stunden pro tag (pro label hat das model 24 werte)
+    classCount = len(set(training_labels))  # labels bzw anzahl der Tage times number of load profiles
+    lr = 1e-5
+    maxNorm = 1e6
+    epochCount = 1000
+    # testLabel = 0
+    model = GAN(
+        name="current_model",
+        device=device,
+        batchSize=batchSize,
+        samples=training_samples,
+        labels=training_labels,
+        dimLatent=dimLatent,
+        featureCount=featureCount,
+        classCount=classCount,
+        dimEmbedding=dim_emmbedded,
+        lr=lr,
+        maxNorm=maxNorm,
+        epochCount=epochCount,
+        # testLabel = testLabel
+    )
+
+    model.train()
+
+    # Save model
+    model.name = 'model_test_philipp'
+    print(f"Training {model.name} done""")
+    print("saving model ...")
+    torch.save(model, f'models/{model.name}.pt')
+    print(f"model {model.name} saved")
+
+
+def load_model(model_name="model_test_andi"):
+    import torch
+    import GAN
+    model = torch.load(f"models/{model_name}.pt")
+    array = model.generate_sample()
+    df_synthProfiles = df_profiles.copy()
+    df_synthProfiles[::] = array
+    df2 = df_synthProfiles.reset_index().melt(id_vars=["date", "profile"]).pivot_table(values="value",
+                                                                                       columns="profile", index=["date",
+                                                                                                                 "hour of the day"])
+    df2.to_csv(f"{model_name}_synthetic.csv")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='launches test torch')
+    parser.add_argument("--password", default="Ene123Elec#4")
+    args = parser.parse_args()
+
+    password = args.password
+    if password == "":
+        password = getpass.getpass('Password: ')
+        print("No password given. Exit now.")
+        sys.exit()
+
+    cluster_label = 1
+
+    dim_embed = 100
+    pid = (os.getpid())
+    print(pid)
+
+    for i in [30]:
+        print(f"Size  {i=}")
+        main(password_=password,
+             number_of_profiles=i,
+             dim_emmbedded=dim_embed,
+             clusterLabel=0,
+             label_csv_filename="DBSCAN_15_clusters_labels.csv")
+        torch.cuda.empty_cache()
+
+os.kill(pid, signal.SIGTERM)
+sys.exit()
